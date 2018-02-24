237081bae35db4441fb1831d49e15fe96cf16c0f
Viktor Dmitriev <vitaminnus@gmail.com>
2018-02-24 17:28:01 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    };
+    if (currency <= 0) {
+        return {};
+    };
+    const result = {};
+    const coins = {"H":50, "Q":25, "D":10, "N":5, "P":1};
+    let count = currency;
+    Object.keys(coins).forEach(elem => {
+        let c = coins[elem];
+        const coin = Math.floor(count/c);
+        count = count - coin*c;
+        if (coin > 0) {
+            result[elem] = coin;
+        };
+    });
+    return result;
 }
